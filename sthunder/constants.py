703ecdf929c3cdee9423f1ae513a2e4118a22376
Adriano P. Almeida <adriano.almeida@inpe.br>
2021-09-09 01:41:41 -0300

    Update constants file

---
+++ sthunder/constants.py
@@ -0,0 +1,12 @@
+SHP_SOUTH_AMERICA = "/home/adriano/CAP-395/data/GEO/Vector/SouthAmericaPolygon/South_America.shp"
+SHP_BRAZIL_REGIONS = "/home/adriano/CAP-395/data/GEO/Vector/BrazilRegionsPolygon/regioes_2010.shp"
+SHP_BRAZIL_STATES = "/home/adriano/CAP-395/data/GEO/Vector/brazil-uf-shapefile/estados_2010.shp"
+SHP_BRAZIL_CITIES = "/home/adriano/CAP-395/data/GEO/Vector/BrazilCitiesPolygon/municipios_2010.shp"
+
+DIR_GLM_FILES = "/glm/G05GT1H"
+DIR_IMG_CITIES = "/home/adriano/sthunder/results/city"
+
+STYLE_TITLE = {'size': 16, 'weight': 'bold'}
+
+TITLE_SINGLE_DENSITY_CITY_MAP = "DENSIDADE DE FLASHES POR MUNICÍPIOS"
+
